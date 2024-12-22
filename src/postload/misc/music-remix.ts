cc5324acdc65e0d014e39e41fc0ab771af44c2cd
Guilherme Ramos <guilherme.hor@gmail.com>
2024-12-22 03:58:29 -0300

    Fixed remix selector when entering some maps
    Entering autumn-fall north exit made the game crash.
    Checking if this.remixes[bgm] also is undefined, solved the issue.

--- src/postload/misc/music-remix.ts
+++ src/postload/misc/music-remix.ts
@@ -1,43 +1,44 @@
 el.MusicRemixController = ig.Class.extend({
-    remixes: {},
-    selectedRemixes: {
-    },
+	remixes: {},
+	selectedRemixes: {},
 
-    init() {
-        this.loadFromStorage();
-    },
+	init() {
+		this.loadFromStorage();
+	},
 
-    registerRemix(originalTrack, trackReplacement, data) {
-        this.remixes[originalTrack] ??= {};
+	registerRemix(originalTrack, trackReplacement, data) {
+		this.remixes[originalTrack] ??= {};
 
-        this.remixes[originalTrack][trackReplacement] = {
-            replacement: trackReplacement,
+		this.remixes[originalTrack][trackReplacement] = {
+			replacement: trackReplacement,
 
-            name: data?.name ?? { en_US: trackReplacement },
-            desc: data?.desc ?? { en_US: " " },
-        }
-    },
+			name: data?.name ?? { en_US: trackReplacement },
+			desc: data?.desc ?? { en_US: " " },
+		}
+	},
 
-    getRemix(bgm) {
-        //verify there is a remix associated and if there is, make sure it actually is valid
-        if (this.selectedRemixes[bgm] && (this.selectedRemixes[bgm] in this.remixes[bgm])) {
-            return this.selectedRemixes[bgm];
-        } else {
-            //otherwise, it's clearly invalid. we'll just delete it then.
-            if (this.selectedRemixes[bgm]) {
-                delete this.selectedRemixes[bgm];
-                this.saveToStorage();
-            }
-            return bgm;
-        }
-    },
+	getRemix(bgm) {
+		//verify there is a remix associated and if there is, make sure it actually is valid
+		if ((this.selectedRemixes[bgm] && this.remixes[bgm]) &&
+			this.selectedRemixes[bgm] in this.remixes[bgm]
+		) {
+			return this.selectedRemixes[bgm];
+		} else {
+			//otherwise, it's clearly invalid. we'll just delete it then.
+			if (this.selectedRemixes[bgm]) {
+				delete this.selectedRemixes[bgm];
+				this.saveToStorage();
+			}
+			return bgm;
+		}
+	},
 
-    setRemix(bgm, newBgm) {
-        if (newBgm) {
-            this.selectedRemixes[bgm] = newBgm;
-        } else {
-            delete this.selectedRemixes[bgm];
-        }
+	setRemix(bgm, newBgm) {
+		if (newBgm) {
+			this.selectedRemixes[bgm] = newBgm;
+		} else {
+			delete this.selectedRemixes[bgm];
+		}
 
         //kinda hacky, but it works.
         //any already set ones will be loaded in poststart.
